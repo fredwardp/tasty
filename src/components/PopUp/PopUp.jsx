78c484de335ec0ae880d197c7358606403f45cf1
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-03-11 09:11:37 +0100

    home mobile version fast fertig

---
+++ src/components/PopUp/PopUp.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./PopUp.css";
+const PopUp = (props) => {
+  console.log(props.data);
+
+  return (
+    <div className="popup_link_container">
+      {props.data ? (
+        props.data.meals.map((item, index) => (
+          <div className="popup_link_wrapper">
+            <Link key={index} className="popup_link">
+              {item[Object.keys(item)[0]]}
+            </Link>
+          </div>
+        ))
+      ) : (
+        <p>Loading..</p>
+      )}
+    </div>
+  );
+};
+
+export default PopUp;
