0db20ebb72dce5e265c9f25bb2658150bef0711b
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-03-12 11:29:49 +0100

    app.jsx pfade angepasst

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { CloseContext, SendDataContext, SplashContext, SearchValueContext } from "./context/context";
+import {
+  CloseContext,
+  SendDataContext,
+  SplashContext,
+  SearchValueContext,
+} from "./context/context";
 import { useState } from "react";
 import Home from "./pages/Home/Home";
 import Onboarding from "./pages/Onboarding/Onboarding";
@@ -12,36 +17,45 @@ import SplashScreen from "./pages/SplashScreen/SplashScreen";
 SplashContext;
 
 function App() {
-    const [splash, setSplash] = useState(false);
-    const [close, setClose] = useState(false);
-    const [sendData, setSendData] = useState(false);
-    const [searchValue, setSearchValue] = useState();
+  const [splash, setSplash] = useState(false);
+  const [close, setClose] = useState(false);
+  const [sendData, setSendData] = useState(false);
+  const [searchValue, setSearchValue] = useState();
 
-    return (
-        <SearchValueContext.Provider value={{ searchValue, setSearchValue }}>
-            <SendDataContext.Provider value={{ sendData, setSendData }}>
-                <CloseContext.Provider value={{ close, setClose }}>
-                    <SplashContext.Provider value={{ splash, setSplash }}>
-                        {/* Ternary um SplashScreen für einige Sekunden anzuzeigen */}
-                        {splash ? (
-                            <BrowserRouter>
-                                <Routes>
-                                    <Route path="/" element={<Home />} />
-                                    <Route path="/onboarding" element={<Onboarding />} />
-                                    <Route path="/searchsite/areas" element={<SearchSite_Areas />} />
-                                    <Route path="/searchsite/categories" element={<SearchSite_Categories />} />
-                                    <Route path="/searchsite/results/:search" element={<SearchSite_Results />} />
-                                    <Route path="/details/:id" element={<Details />} />
-                                </Routes>
-                            </BrowserRouter>
-                        ) : (
-                            <SplashScreen />
-                        )}
-                    </SplashContext.Provider>
-                </CloseContext.Provider>
-            </SendDataContext.Provider>
-        </SearchValueContext.Provider>
-    );
+  return (
+    <SearchValueContext.Provider value={{ searchValue, setSearchValue }}>
+      <SendDataContext.Provider value={{ sendData, setSendData }}>
+        <CloseContext.Provider value={{ close, setClose }}>
+          <SplashContext.Provider value={{ splash, setSplash }}>
+            {/* Ternary um SplashScreen für einige Sekunden anzuzeigen */}
+            {splash ? (
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/onboarding" element={<Onboarding />} />
+                  <Route
+                    path="/searchsite/areas/:area"
+                    element={<SearchSite_Areas />}
+                  />
+                  <Route
+                    path="/searchsite/categories/:categ"
+                    element={<SearchSite_Categories />}
+                  />
+                  <Route
+                    path="/searchsite/results/:search"
+                    element={<SearchSite_Results />}
+                  />
+                  <Route path="/details/:id" element={<Details />} />
+                </Routes>
+              </BrowserRouter>
+            ) : (
+              <SplashScreen />
+            )}
+          </SplashContext.Provider>
+        </CloseContext.Provider>
+      </SendDataContext.Provider>
+    </SearchValueContext.Provider>
+  );
 }
 
 export default App;
