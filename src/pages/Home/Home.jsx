b2d7e172b27d54689daf6820353bc35b26abb6ad
MichaelGee76 <gottungmichael@gmail.com>
2024-03-12 09:07:48 +0100

    updt

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,110 +8,110 @@ import PopUp from "../../components/PopUp/PopUp";
 import All_Areas from "../../components/All_Areas/All_Areas";
 import SearchBar from "../../components/SearchBar/SearchBar";
 const Home = () => {
-  const [random, setRandom] = useState();
-  const [fullCategories, setFullCategories] = useState();
-  const [areas, setAreas] = useState();
-  const { close, setClose } = useContext(CloseContext);
-  const { sendData, setSendData } = useContext(SendDataContext);
-  const [categories, setCategories] = useState();
+    const [random, setRandom] = useState();
+    const [fullCategories, setFullCategories] = useState();
+    const [areas, setAreas] = useState();
+    const { close, setClose } = useContext(CloseContext);
+    const { sendData, setSendData } = useContext(SendDataContext);
+    const [categories, setCategories] = useState();
 
-  useEffect(() => {
-    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-      .then((response) => response.json())
-      .then((randomMeal) => setRandom(randomMeal))
-      .catch((error) => console.log("keine Daten vorhanden", error));
-  }, []);
+    useEffect(() => {
+        fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+            .then((response) => response.json())
+            .then((randomMeal) => setRandom(randomMeal))
+            .catch((error) => console.log("keine Daten vorhanden", error));
+    }, []);
 
-  useEffect(() => {
-    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
-      .then((response) => response.json())
-      .then((fullCategories) => setFullCategories(fullCategories))
-      .catch((error) => console.log("keine Categorie Daten vorhanden", error));
-  }, []);
+    useEffect(() => {
+        fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+            .then((response) => response.json())
+            .then((fullCategories) => setFullCategories(fullCategories))
+            .catch((error) => console.log("keine Categorie Daten vorhanden", error));
+    }, []);
 
-  useEffect(() => {
-    fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
-      .then((response) => response.json())
-      .then((allAreas) => setAreas(allAreas))
-      .catch((error) => console.log("keine Area Daten vorhanden", error));
-  }, []);
+    useEffect(() => {
+        fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+            .then((response) => response.json())
+            .then((allAreas) => setAreas(allAreas))
+            .catch((error) => console.log("keine Area Daten vorhanden", error));
+    }, []);
 
-  useEffect(() => {
-    fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
-      .then((response) => response.json())
-      .then((allCategories) => setCategories(allCategories))
-      .catch((error) => console.log("keine Area Daten vorhanden", error));
-  }, []);
+    useEffect(() => {
+        fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
+            .then((response) => response.json())
+            .then((allCategories) => setCategories(allCategories))
+            .catch((error) => console.log("keine Area Daten vorhanden", error));
+    }, []);
 
-  const categoriesOnClick = () => {
-    setClose((close) => !close);
-    console.log(sendData);
-  };
+    const categoriesOnClick = () => {
+        setClose((close) => !close);
+        console.log(sendData);
+    };
 
-  const closeFunc = () => {
-    if (sendData == true) {
-      setSendData((sendData) => !sendData);
-    }
-    setClose((close) => !close);
-  };
+    const closeFunc = () => {
+        if (sendData == true) {
+            setSendData((sendData) => !sendData);
+        }
+        setClose((close) => !close);
+    };
 
-  return (
-    <div className="home_all_wrapper">
-      <section className={`popup_section ${close ? "" : "close"}`}>
-        <article className="popup_content">
-          <div onClick={closeFunc} className="close_div">
-            <div className="close_line"></div>
-            <div className="close_line"></div>
-          </div>
-          <h2>{sendData ? "All Areas" : "All Categories"}</h2>
-          <PopUp data={sendData ? areas : categories} />
-        </article>
-      </section>
-      <header className="home_header">
-        <div className="home_search_wrapper">
-          <SearchBar />
-        </div>
-        <h1>Meal of the Day</h1>
-        {random ? (
-          <div className="meal_of_day">
-            <h2>{random.meals[0].strMeal}</h2>
-            <div className="mod_lower_wrapper">
-              <div>
-                <div className="home_round_grey"></div>
-                {random.meals[0].strCategory}
-              </div>
-              <p>{random.meals[0].strArea}</p>
-            </div>
-          </div>
-        ) : (
-          <p>Loading..</p>
-        )}
-        <All_Areas data={areas} />
-        <div className="home_h2_wrapper">
-          <h2>Categories</h2>
-          <p onClick={categoriesOnClick}>See All</p>
-        </div>
-      </header>
-      <section className="home_categories_section">
-        <div className="categories_slider">
-          {fullCategories ? (
-            fullCategories.categories.map((item, index) => (
-              <Link>
-                <article key={index}>
-                  <img src={item.strCategoryThumb} alt="" />
-                  <h3>{item.strCategory}</h3>
+    return (
+        <div className="home_all_wrapper">
+            <section className={`popup_section ${close ? "" : "close"}`}>
+                <article className="popup_content">
+                    <div onClick={closeFunc} className="close_div">
+                        <div className="close_line"></div>
+                        <div className="close_line"></div>
+                    </div>
+                    <h2>{sendData ? "All Areas" : "All Categories"}</h2>
+                    <PopUp data={sendData ? areas : categories} />
                 </article>
-              </Link>
-            ))
-          ) : (
-            <p>Keine Daten vorhanden</p>
-          )}
-        </div>
-      </section>
+            </section>
+            <header className="home_header">
+                <div className="home_search_wrapper">
+                    <SearchBar />
+                </div>
+                <h1>Meal of the Day</h1>
+                {random ? (
+                    <div className="meal_of_day">
+                        <h2>{random.meals[0].strMeal}</h2>
+                        <div className="mod_lower_wrapper">
+                            <div>
+                                <div className="home_round_grey"></div>
+                                {random.meals[0].strCategory}
+                            </div>
+                            <p>{random.meals[0].strArea}</p>
+                        </div>
+                    </div>
+                ) : (
+                    <p>Loading..</p>
+                )}
+                <All_Areas data={areas} />
+                <div className="home_h2_wrapper">
+                    <h2>Categories</h2>
+                    <p onClick={categoriesOnClick}>See All</p>
+                </div>
+            </header>
+            <section className="home_categories_section">
+                <div className="categories_slider">
+                    {fullCategories ? (
+                        fullCategories.categories.map((item, index) => (
+                            <Link>
+                                <article key={index}>
+                                    <img src={item.strCategoryThumb} alt="" />
+                                    <h3>{item.strCategory}</h3>
+                                </article>
+                            </Link>
+                        ))
+                    ) : (
+                        <p>Keine Daten vorhanden</p>
+                    )}
+                </div>
+            </section>
 
-      <Nav />
-    </div>
-  );
+            <Nav />
+        </div>
+    );
 };
 
 export default Home;
--- src/pages/SearchSite_Results/SearchSite_Results.jsx
+++ src/pages/SearchSite_Results/SearchSite_Results.jsx
@@ -2,6 +2,7 @@ import Nav from "../../components/Nav/Nav";
 import "./SearchSite_Results.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const SearchSite_Results = () => {
     const [searchValue, setSearchValue] = useState("");
@@ -17,27 +18,33 @@ const SearchSite_Results = () => {
     console.log("My Data", myData);
     return (
         <>
+            {/* ! ???? ! Abfragen ob array == 0/null dann Fehlermeldung auf Screen ausgeben */}
+            {myData <= 0 ? <p>No Data</p> : <p>Data</p>}
             <div className="searchResultsContainer">
                 <SearchBar setSearchValue={setSearchValue} />
                 {myData ? (
-                    <p>
+                    <div>
                         <ul>
                             {myData.meals.map((meal, index) => (
-                                <div key={index} className="tile">
-                                    <img src={meal.strMealThumb} alt={`image of ${meal.strMeal}`} />
-                                    <div className="innerTile">
+                                <div key={index} className="searchSiteResultTile">
+                                    <img src={meal.strMealThumb} className="searchSiteResultImage" alt={`image of ${meal.strMeal}`} />
+                                    <div className="searchSiteResultInnerTile">
                                         <h3>{meal.strMeal}</h3>
-                                        <div>
+                                        <div className="searchSiteResultsInnerDiv">
                                             <div className="searchSiteDot"></div>
-                                            <p>{searchValue}</p>
+                                            <p>{searchValue.toUpperCase()}</p>
                                         </div>
+                                    </div>
+                                    <div className="searchSiteResultLinkDiv">
+                                        {/* Link to detail? Wert idMeal??? */}
+                                        <Link to={`"/details/${meal.idMeal}`}>🍆</Link>
                                     </div>
                                 </div>
                             ))}
                         </ul>
-                    </p>
+                    </div>
                 ) : (
-                    <h2 style={{ color: "red" }}>No Data found. Try again</h2>
+                    <h2 style={{ color: "red" }}>Data loading...</h2>
                 )}
             </div>
 
